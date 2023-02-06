e8e3aa0c9a65f7dbd7577a1d5217f82e327dcfc2
DmitriyAgapov <prweb2012@gmail.com>
2023-02-06 17:42:40 +0500

    first

--- config/plugins.js
+++ config/plugins.js
@@ -2,6 +2,13 @@ module.exports = () => ({
   'duplicate-button': true,
   "vercel-deploy": {
     enabled: true,
+    config: {
+      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
+      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
+      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+      // teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_TEAM_FILTER,
+      roles: ["strapi-super-admin"],
+    },
   },
   slugify: {
     enabled: true,
